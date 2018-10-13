f73c540ee40bc580d40db98f744c07a1fb9dfda4
Unknown <nasir016@gmail.com>
2018-10-13 16:27:54 +0200

    Renamed project.

--- wp-frontend-angular/src/app/app.module.ts
+++ wp-frontend-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WebPageComponent } from './web-page/web-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebPageComponent
   ],
   imports: [
     BrowserModule
---
+++ wp-frontend-angular/src/app/web-page/web-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-web-page',
+  templateUrl: './web-page.component.html',
+  styles: []
+})
+export class WebPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
